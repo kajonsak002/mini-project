e617aa9fddd0a0eb5e91bd562766b46dc987b8ef
kajonsak002 <651463002@crru.ac.th>
2023-04-25 12:44:33 +0700

    React mini Project

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import "./Footer.scss";
+
+type Props = {};
+
+const Footer = ({}: Props) => {
+  return (
+    <footer>
+      <p>@Copyright 2023  Kajonsak Leepongkul </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/NotFound/index.tsx
@@ -0,0 +1,17 @@
+
+type Props = {};
+
+const NotFound = ({}: Props) => {
+  return (
+    <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh'
+    }}>
+        <h3>404 Page NotFound</h3>
+    </div>
+  );
+};
+
+export default NotFound;
